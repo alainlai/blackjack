c228c2dcb33d0cbaecb0856c2743766c8c66b7cb
alainlai <alainlai2@gmail.com>
2023-10-04 19:03:51 +0800

    Adding more logic

--- index.js
+++ index.js
@@ -1,42 +1,39 @@
-let messageEl = document.getElementById("message-el")
-let cardsEl = document.getElementById("cards-el")
-let sumEl = document.querySelector("#sum-el")
-let winMsg = document.getElementById("win-msg")
-
+//
+let cards = []
+let sum = 0
 let haveBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.getElementById("cards-el")
 
-let firstCard = Math.floor(Math.random() * 10) + 2
-let secondCard = Math.floor(Math.random() * 10) + 2
-let cards = [firstCard,secondCard]
-
-let card = 0
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
 
 function startGame() {
-    if (isAlive == false) {
-        firstCard = Math.floor(Math.random() * 10) + 2
-        secondCard = Math.floor(Math.random() * 10) + 2
-        cards = [firstCard,secondCard]
-        sum = 0
-        messageEl.textContent = "New game"
-        isAlive = true
-    }
-
-    if (haveBlackJack == true) {
-        firstCard = Math.floor(Math.random() * 10) + 2
-        secondCard = Math.floor(Math.random() * 10) + 2
-        cards = [firstCard,secondCard]
-        sum = 0
-        messageEl.textContent = "New game"
-        haveBlackJack = false
-    }
-
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard,secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 
 function renderGame() {
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i ++) {
+        cardsEl.textContent = cardsEl.textContent + " " + cards[i]
+    }
+
     sumEl.textContent = "Sum: " + sum
     
     if (sum < 21) {
@@ -54,7 +51,7 @@ function renderGame() {
 function newCard() {
     message = "Drawing a new card"
     messageEl.textContent = message
-    card = Math.floor(Math.random() * 10) + 2
+    card = getRandomCard()
     
     sum = sum + card
     cards.push(card)
